acbbfa9b6d9f6069fb4b30e2f88501e032c9bfde
naarkarrrr <shivamnarkar04@gmail.com>
2023-09-10 16:21:28 +0530

    added meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,16 @@ function MyApp({ Component, pageProps }) {
     <Fragment>
       <Head>
         {/* <!-- Google Fonts --> */}
+        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700;800;900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+          <meta name="description" content="Branding catalyst  digital marketing service that can help you create, launch, and grow your online presence with a range of services, from research and insight, to design sprint, to product management, to technology strategy, to user interface (UI), to user experience (UX), to user testing, to interaction design (IXD), to prototyping, to Android apps development, to iOS apps development, to enterprise software development, to web apps development, to data visualization, to support, to analytics, to adoption, to DevOps, to engagement, to retention, to blockchain, to data science, to internet of things (IoT), to artificial intelligence (AI), to machine learning (ML), and to meta verse (XR). Rudra Trivedi DJ sanghvi"/>
+            <meta name="keywords" content="branding catalyst,branding catalyst vasai, branding catalyst Andheri, brandingcatalyst.com, branding catalyst.net,Branding Catalyst Pvt Ltd, digital marketing service provider,digital marketing solutions,digital marketing consulting,research and insight,user interface,user experience,data visualization,data science,internet of things,machine learning, design sprint, product management, technology strategy, user testing, interaction design, prototyping, Android apps development, iOS apps development, enterprise software development, web apps development, support, analytics, adoption, DevOps, engagement, retention, blockchain, artificial intelligence, meta verse"/>
+            <meta name="author" content="Branding Catalyst"/>  
         {/* <!-- !Google Fonts --> */}
 
         {/* <!-- Styles --> */}
